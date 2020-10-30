2eda2b4136502005ea3706a2ab97ae0d0f3e2486
ReluctantPirate <reluctantpirategames@gmail.com>
2020-10-30 14:37:02 -0400

    Revert "5766 lift function call"
    This reverts commit 526ad52b64dcf0279b078dfed389de1a142e6993.

--- DragonsLair/DragonsLair.ino
+++ DragonsLair/DragonsLair.ino
@@ -243,13 +243,9 @@ void inertLoop() {
     }
     //listen for damage
     FOREACH_FACE(f) {
-      
       if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
-
-        byte lastValueReceived = getLastValueReceivedOnFace(f);
-        
-        if (getBlinkType(lastValueReceived) == FIELD) {
-          if (getAttackSignal(lastValueReceived) == FIRE || getAttackSignal(lastValueReceived) == POISON) {
+        if (getBlinkType(getLastValueReceivedOnFace(f)) == FIELD) {
+          if (getAttackSignal(getLastValueReceivedOnFace(f)) == FIRE || getAttackSignal(getLastValueReceivedOnFace(f)) == POISON) {
             if (ignoredFaces[f] == 0) {//take damage
               luck--;
               damageAnimTimer.set(DAMAGE_ANIM_TIME);
